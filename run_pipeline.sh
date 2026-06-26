6b545dc7dea4294988edcea5d77c43344f924f58
Jorge Guzman <jag2367@gsb.columbia.edu>
2026-06-26 09:28:40 -0400

    udpates to pipeline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,3 +1,7 @@
-grid_run --grid_mem=300G --grid_ncpus=6 /user/jag2367/.conda/envs/jgpriv/bin/python Create_Pandas_Datasets.py 
-grid_run --grid_mem=300G --grid_ncpus=6 /user/jag2367/.conda/envs/jgpriv/bin/python Match_to_SCP_Data.py 
-grid_run --grid_mem=300G --grid_ncpus=6 /user/jag2367/.conda/envs/jgpriv/bin/python Create_Stata_Analysis_Panel.py 
+set -o pipefail
+
+{
+  grid_run --grid_mem=300G --grid_ncpus=6 /user/jag2367/.conda/envs/jgpriv/bin/python Create_Pandas_Datasets.py
+  grid_run --grid_mem=300G --grid_ncpus=6 /user/jag2367/.conda/envs/jgpriv/bin/python Match_to_SCP_Data.py
+  grid_run --grid_mem=300G --grid_ncpus=6 /user/jag2367/.conda/envs/jgpriv/bin/python Create_Stata_Analysis_Panel.py
+} 2>&1 | tee run_pipeline.log
